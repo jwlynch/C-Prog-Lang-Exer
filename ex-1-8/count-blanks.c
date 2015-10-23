d891e0c73028c7a622ba08e9d3a4bf7c382aa725
Jim Lynch <jim@jam.sessionsnet.org>
2015-10-23 16:13:19 -0700

    count blanks, tabs and newlines separately, and report each

--- ex-1-8/count-blanks.c
+++ ex-1-8/count-blanks.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 
+// ex-1-9.c
+//
+// write a program to count blanks, tabs and newlines.
+
 int main(void)
 {
-  int ch = getchar(), blank_count = 0;
+  int blank_count = 0;
+  int tab_count = 0;
+  int newline_count = 0;
+  
+  int ch = getchar();
   
   while( ch != EOF )
   {
     if( ch == ' ' )
       ++blank_count;
     else if( ch == '\t' )
-      ++blank_count;
+      ++tab_count;
     else if( ch == '\n' )
-      ++blank_count;
+      ++newline_count;
     
     ch = getchar();
   }
   
-  printf("there were %d blanks.\n", blank_count);
+  printf
+  (
+    "there were %d blanks, %d tabs and %d newlines.\n", 
+   blank_count, 
+   tab_count, 
+   newline_count
+  );
   
   return 0;
 }
